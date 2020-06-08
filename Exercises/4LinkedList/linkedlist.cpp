4d11b15c21a1a1dbcf7fdbdbd2ad0aad3cb62125
Xinyu Jian <jianxinyu626@gmail.com>
2020-06-08 15:26:49 -0230

    update linked list

--- Exercises/4LinkedList/linkedlist.cpp
+++ Exercises/4LinkedList/linkedlist.cpp
@@ -14,29 +14,35 @@ public:
     }
 
     //add a new node at the end of the linked list
-    void append(T data)
+    LinkedList& operator << (T x)
     {
-        Node *tmp = new Node(data);
-        //if the list is empty, head_ and tail_ will be the same node
-        if(head_ == nullptr)
+        auto tmp = std::make_unique<Node>(std::move(x));
+        //check whether its the first node
+        if(tail_)
         {
-            head_ = tmp;
-            tail_ = tmp;
+            tail_->append(std::move(tmp));
+            tail_ = tail_->next();
         }
-        else //add the node at the end of the linked list
+        else
         {
-            tail_->next_ = tmp;
-            tail_ = tail_->next_;
+            head_ = std::move(tmp);
+            tail_ = head_.get();
         }
+        //operators usually need to return reference to something
+        //returning *this makes cascade usage possible
+        //e.g. someList << 1 << 2 << 3;
+        return *this;
     }
 
     //display contents of linked list
     void display()
     {
-        Node *pnode = head_; //present node
-        while(pnode != nullptr) {
-            std::cout << pnode->value_ << "-->";
-            pnode = pnode->next_;
+        Node *pnode = head_.get(); //present node
+        //keep printing node until the tail_
+
+        while(pnode) {
+            std::cout << *pnode->value_ << "-->";
+            pnode = pnode->next_.get();
         }
         std::cout << "Null \n";
     }
@@ -45,47 +51,60 @@ private:
     class Node
     {
     public:
-        explicit Node(T value)
-            : value_(value), next_(nullptr)
+        Node(T value)
+            : next_(nullptr), prev_(nullptr)
         {
+            value_ = std::move(value);
         }
 
-        T      value_;
-        Node   *next_;
+        void append(std::unique_ptr<Node> n)
+        {
+            next_ = std::move(n);
+            next_->prev_ = this;
+        }
+
+        Node* next()
+        {
+            return next_.get();
+        }
+
+        T                           value_;
+        std::unique_ptr<Node>       next_;
+        Node                        *prev_;
     };
-    Node       *head_;
-    Node       *tail_;
+    std::unique_ptr<Node>           head_;
+    Node                            *tail_;
 };
 
+//template <typename T>
+//std::ostream& operator<<(std::ostream &os, const  LinkedList<std::unique_ptr<T>> &list)
+//{
+//
+//}
 int main() {
-    //test integer
-    LinkedList<int> listInt;
-    listInt.append(1);
-    listInt.append(2);
-    std::cout << "Int Linked List: ";
-    listInt.display();
-
-    //test double
-    LinkedList<double> listDbl;
-    listDbl.append(1.1);
-    listDbl.append(2.2);
-    std::cout << "Double Linked List: ";
-    listDbl.display();
-
-    //test string
-    LinkedList<std::string> listStr;
-    listStr.append("Alice");
-    listStr.append("Bob");
-    std::cout << "String Linked List: ";
-    listStr.display();
+//    //test integer
+//    LinkedList<int> listInt;
+//    listInt << 1 << 2;
+//    std::cout << "Int Linked List: ";
+//    listInt.display();
+//
+//    //test double
+//    LinkedList<double> listDbl;
+//    listDbl << 1.1 << 2.2;
+//    std::cout << "Double Linked List: ";
+//    listDbl.display();
+//
+//    //test string
+//    LinkedList<std::string> listStr;
+//    listStr << "Alice" << "Bob";
+//    std::cout << "String Linked List: ";
+//    listStr.display();
 
     //test non-copyable type
-    LinkedList<std::unique_ptr<double>> listNC;
-    double a = 1.1, b = 2.2;
-    std::unique_ptr<double> ptr_a(&a), ptr_b(&b);
-    listNC.append(std::move(ptr_a));
-
-
+    LinkedList<std::unique_ptr<double>> pointers;
+    pointers << std::make_unique<double>(1.1) << std::make_unique<double>(2.2);
+    std::cout << "Unique Pointer Linked List: ";
+    pointers.display();
 
    return 0;
 }
